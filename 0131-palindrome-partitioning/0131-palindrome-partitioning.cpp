5c2960065bbd2ea9445e33d7aa037ffe680e3302
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-05-31 22:15:45 +0530

    Time: 52 ms (46.89%), Space: 79.9 MB (28.72%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    void fun(int idx, string &s, vector<string> &path) {
+        if (idx == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = idx; i < s.size(); i++) {
+            if (isPalindrome(s, idx, i)) {
+                path.push_back(s.substr(idx, i - idx + 1));
+                fun(i + 1, s, path);
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<string> path;
+        fun(0, s, path);
+        return ans;
+    }
+};
