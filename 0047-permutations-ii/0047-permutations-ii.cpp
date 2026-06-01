fc863c18be01d7ab6e490778d7b1e00a7fdb4d73
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-06-01 11:45:35 +0530

    Time: 8 ms (23.91%), Space: 15.8 MB (7.4%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void fun(int i, vector<int>& nums) {
+        if (i == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> used;
+
+        for (int j = i; j < nums.size(); j++) {
+            if (used.count(nums[j]))
+                continue;
+
+            used.insert(nums[j]);
+
+            swap(nums[i], nums[j]);
+            fun(i + 1, nums);
+            swap(nums[i], nums[j]);
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        fun(0, nums);
+        return ans;
+    }
+};
