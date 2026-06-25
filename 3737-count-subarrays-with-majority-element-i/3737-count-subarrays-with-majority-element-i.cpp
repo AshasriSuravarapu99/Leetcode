499113fa831ea7bb7352c480a9d9b5f43b62ccc2
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-06-25 11:01:48 +0530

    Time: 38 ms (71.24%), Space: 39.5 MB (75.6%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int>pre(n,0);
-        if(nums[0] == target) pre[0] = 1;
-        else pre[0] = 0;
-        for(int i = 1; i < n; i++){
-            if(nums[i] == target) pre[i] = pre[i-1] + 1;
-            else pre[i] = pre[i-1];
-        }
-
         int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                int req = (j-i+1)/2;
-                if(i == 0 && pre[j] > req) ans++;
-                else if(i != 0 && (pre[j] - pre[i-1] > req)) ans++;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
             }
         }
-
         return ans;
     }
 };
