1c872b4817b6be7c5cb0182f9a5088d2afe43677
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-07-24 09:25:37 +0530

    Time: 3 ms (69.76%), Space: 12.9 MB (90.26%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void construct(string s, int n, vector<string> &res, int oc, int cc){
-        if(oc == n && cc == n){
+    void construct(string &s, int n, vector<string> &res, int oc, int cc) {
+        if (oc == n && cc == n) {
             res.push_back(s);
             return;
         }
-
-        if(oc < n){
-            construct(s+'(',n,res,oc+1,cc);
+        if (oc < n) {
+            s.push_back('(');
+            construct(s, n, res, oc + 1, cc);
+            s.pop_back(); 
         }
-        if(cc < oc){
-            construct(s+')', n, res, oc, cc+1);
+        if (cc < oc) {
+            s.push_back(')');
+            construct(s, n, res, oc, cc + 1);
+            s.pop_back(); 
         }
-
-        return;
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>res;
-        construct("",n,res,0,0);
+        vector<string> res;
+        string s = "";
+        construct(s, n, res, 0, 0);
         return res;
     }
 };
