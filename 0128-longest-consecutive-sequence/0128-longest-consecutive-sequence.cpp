f58f87c550ea2dcf68ccd518a1fb0e3b0d9b20b3
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-07-29 08:15:51 +0530

    Time: 83 ms (48.29%), Space: 88.8 MB (67.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        map<int,int>m;
-        for(int i=0;i<nums.size();i++) m[nums[i]]++;
-        int val;
+
+        unordered_set<int> st(nums.begin(), nums.end());
         int ans = 0;
-        int c=0;
-        int flag = 0;
-        for(auto it:m)
-        {
-            if(flag == 0) 
-               {
-                    val = it.first;
-                    flag = 1;
-               }
-            else if(it.first-val == 1 || it.first-val == -1) 
-            {
-                c++;
-                val = it.first;
-            }
-            else
-            {
-                ans = max(ans,c);
-                c=0;
+
+        for (int x : st) {
+            if (st.find(x - 1) == st.end()) {
+
+                int current = x;
+                int c = 1;
+
+                while (st.find(current + 1) != st.end()) {
+                    c++;
+                    current++;
+                }
+
+                ans = max(ans, c);
             }
         }
-        // for(auto it:m) cout<<it.first<<" "<<it.second<<"\n";
-        ans  = max(ans,c)+1;
-        if(m.size() == 0 || m.size() == 1) return m.size();
+
         return ans;
-        // return 0;
     }
 };
