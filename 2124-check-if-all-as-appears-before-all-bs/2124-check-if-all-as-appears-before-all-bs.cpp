9c0d7524d1eae93bf06ddd9f32a51287d1ebd4ed
AshasriSuravarapu99 <155606512+AshasriSuravarapu99@users.noreply.github.com>
2026-07-30 10:36:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (99.06%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        int n = s.size();
+        int count = 0;
+        for(int i = 0; i < n-1; i++){
+            if(s[i] == 'b' && s[i+1] == 'a') count++;
+        }
+        return count == 0;
+    }
+};
